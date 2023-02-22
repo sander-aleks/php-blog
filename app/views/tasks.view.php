e0c01bc669d1130e3c7e238675fc7037b489dbd5
Sander-Aleks Mander <93116819+sander-aleks@users.noreply.github.com>
2023-02-22 15:36:51 +0200

    Add files via upload

--- app/views/tasks.view.php
+++ app/views/tasks.view.php
@@ -10,27 +10,28 @@
     <input type="text" name="name" placeholder="name">
     <input type="submit" value="Post">
 </form>
-    <?php foreach ( $tasks as $task ): ?>
-        <div class="BlogP">
-            <p>
-                <?php if ( $task->title ): ?>
-                    <h1 class=blogT><?= $task->title; ?></h1>
-                    <?php endif; ?>
-                    <p>Description:</p>
-                <?php if ( $task->description ): ?>
-                    <p class="BlogTx"><?= $task->description; ?></p>
-                    <?php endif; ?>
-                    <p>Time:</p>
-                <?php if  ($task->time ): ?>
-                    <p class="BlogTx"><?= $task->time; ?></p>
-                    <?php endif; ?>
-                    <p>Name:</p>
-                <?php if ( $task->name ): ?>
-                    <p class="BlogTx"><?= $task->name; ?></p>
-                    <?php endif; ?>
-            </p>
-        </div>    
-    <?php endforeach; ?>
-
+<div class="BlogSec">
+        <?php foreach ( $tasks as $task ): ?>
+            <div class="BlogP">
+                <p>
+                    <?php if ( $task->title ): ?>
+                        <h1 class=blogT><?= $task->title; ?></h1>
+                        <?php endif; ?>
+                        <p>Description:</p>
+                    <?php if ( $task->description ): ?>
+                        <p class="BlogTx"><?= $task->description; ?></p>
+                        <?php endif; ?>
+                        <p>Time:</p>
+                    <?php if  ($task->time ): ?>
+                        <p class="BlogTx"><?= $task->time; ?></p>
+                        <?php endif; ?>
+                        <p>Name:</p>
+                    <?php if ( $task->name ): ?>
+                        <p class="BlogTx"><?= $task->name; ?></p>
+                        <?php endif; ?>
+                </p>
+            </div>    
+        <?php endforeach; ?>
+    </div>
 
 <?php require_once('partials/footer.php'); ?>
